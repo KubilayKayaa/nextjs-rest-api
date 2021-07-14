0e2c7d50674295dba363fc99e36b8f03be22c503
Kubilay Kaya <kkubilay24@gmail.com>
2021-07-14 14:18:57 +0300

    Deleted users
    Deleted users

--- pages/users/index.js
+++
@@ -1,9 +0,0 @@
-// import { useRouter } from "next/router";
-
-// export default function Film() {
-//   const router = useRouter();
-//   const { url } = router.query;
-//   console.log(url);
-
-//   return <p>Film: {url}</p>;
-// }
